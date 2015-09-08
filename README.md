4d20e6a00d0366f6933a7a711bbb4c1a43e9db27
Michael Wuergler <wuergler@gmail.com>
2015-09-08 18:51:12 +0100

    it pads! :astonished:

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# zeropad
-Zeropad your integers with n-length padding. 
+# zeropad [![Build Status](https://travis-ci.org/radiovisual/zeropad.svg)](https://travis-ci.org/radiovisual/zeropad)
+> Zeropad your positive integers with n-length padding.
+
+*For super-basic number padding (no length options), see [zeropad-simple](https://github.com/radiovisual/simple-zeropad).*
+
+## Install
+
+```sh
+$ npm install zeropad
+```
+
+## Usage
+```js
+var zeropad = require('zeropad');
+
+zeropad(10, 4);
+// => '0010'
+
+zeropad(4, 10);
+// => '0000000004');
+
+zeropad(9, 4);
+// => '0009');
+
+```
+
+## License
+
+MIT @ [Michael Wuergler](http://numetriclabs.com)
---
+++ test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var assert = require('assert');
+var zp = require('./');
+
+
+describe('zeropad', function(){
+
+    it('should zeropad to the given length', function(done){
+
+        assert.equal(zp(10, 4),  '0010');
+        assert.equal(zp(4, 10),  '0000000004');
+        assert.equal(zp(40, 10), '0000000040');
+        assert.equal(zp(9, 4),   '0009');
+        assert.equal(zp(0, 1),   '0');
+        assert.equal(zp(0, 2),   '00');
+        assert.equal(zp(101, 6), '000101');
+        done();
+
+    });
+
+    it('should error when num is NaN ', function(){
+
+        assert.throws(function(){
+            zp({}, 1);
+        });
+    });
+
+    it('should error when length is NaN ', function(){
+
+        assert.throws(function(){
+            zp(1, {});
+        });
+    });
+
+
+});
